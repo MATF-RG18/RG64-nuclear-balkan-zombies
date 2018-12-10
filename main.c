eded479c9b1e591b39d24d30e37ca73593cdb7a4
jvnmirkov <jvnmirkov@gmail.com>
2018-12-10 01:40:24 +0100

    Cetvrta nedelja

--- main.c
+++ main.c
@@ -24,6 +24,13 @@ static void ontimer(int value);
 
 int main(int argc, char** argv)
 {
+
+	
+
+ 
+
+
+
     /* initialize glut */
     glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_RGB | GLUT_DEPTH | GLUT_DOUBLE);
@@ -42,6 +49,11 @@ int main(int argc, char** argv)
 	glClearColor(0.1, 0.1,0.1,0);
 	glEnable(GL_DEPTH_TEST);
 	glLineWidth(1.25);	
+
+	
+
+
+
 
 	glutMainLoop();
 	
@@ -179,71 +191,175 @@ static void ontimer(int value)
 
 static void on_display(void)
 {
+
+ 
+    GLfloat light_position[] = { 1, 20, 1, 0 };
+
+    GLfloat light_ambient[] = { 0.0, 0.0, 0.0, 1 };
+
+    GLfloat light_diffuse[] = { 0.7, 0.7, 0.7, 1 };
+
+    GLfloat light_specular[] = { 0.9, 0.9, 0.9, 1 };
+
+
+	GLfloat igrac_coeffs[] = { 0, 0, 0, 1 };
+	GLfloat zid_coeffs[] = { 0.36, 0.25, 0.26, 1 };
+	GLfloat pod_coeffs[] = { 0.556863, 0.137255, 0.137255, 1 };
+
+
+  
+    GLfloat specular_coeffs[] = { 1, 1, 1, 1 };
+
+   
+    GLfloat shininess = 30;
+
+
+  
+    glEnable(GL_LIGHTING);
+    glEnable(GL_LIGHT0);
+    glLightfv(GL_LIGHT0, GL_POSITION, light_position);
+    glLightfv(GL_LIGHT0, GL_AMBIENT,  light_ambient);
+    glLightfv(GL_LIGHT0, GL_DIFFUSE,  light_diffuse);
+    glLightfv(GL_LIGHT0, GL_SPECULAR, light_specular);
+
+   
+  
+
+
+
+
+
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 	glColor3f(0,0,1);
 
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
     gluLookAt(
-            5, 10, 15,
+            5, 15, 20,
             5, 0, 5,
             0, 11, 0
         );
 
+
+
     glPushMatrix();
 
-    glColor3f(0.66, 0.66, 0.88);
-    glTranslatef(5, 0-0.50, 5); /*pod*/
-    glScalef(10, 1, 10);
-    glutWireCube(1);
+    glColor3f(0.556863, 0.137255, 0.137255);
+    glTranslatef(7.5, -0.5, 6.5); /*pod*/
+
+	    glMaterialfv(GL_FRONT, GL_AMBIENT,   pod_coeffs);
+    glMaterialfv(GL_FRONT, GL_DIFFUSE,   pod_coeffs);
+    glMaterialfv(GL_FRONT, GL_SPECULAR,  specular_coeffs);
+    glMaterialf(GL_FRONT,  GL_SHININESS, shininess);
+
+    glScalef(15, 1, 13);
+    glutSolidCube(1);
+    
+    glPopMatrix();
+
+   glPushMatrix();
+
+    glColor3f(0.36, 0.25, 0.26);
+    glTranslatef(7.5, 0.50, 0.5); /*zid1*/
+
+	glMaterialfv(GL_FRONT, GL_AMBIENT,   zid_coeffs);
+    glMaterialfv(GL_FRONT, GL_DIFFUSE,   zid_coeffs);
+    glMaterialfv(GL_FRONT, GL_SPECULAR,  specular_coeffs);
+    glMaterialf(GL_FRONT,  GL_SHININESS, shininess);
+
+
+    glScalef(15, 1, 1);
+    glutSolidCube(1);
     
     glPopMatrix();
 
     glPushMatrix();
 
-    glColor3f(0.66, 0.66, 0.66);
-    glTranslatef(5, 1-0.50, 0.5); /*zid1*/
-    glScalef(10, 1, 1);
-    glutWireCube(1);
-    
-    glPopMatrix();
+    glColor3f(0.36, 0.25, 0.26);
+    glTranslatef(7.5, 1-0.50, 0.5+12); /*zid2*/
 
-    glPushMatrix();
-
-    glColor3f(0.66, 0.66, 0.66);
-    glTranslatef(5, 1-0.50, 0.5+9); /*zid2*/
-    glScalef(10, 1, 1);
-    glutWireCube(1);
-    
-    glPopMatrix();
+	glMaterialfv(GL_FRONT, GL_AMBIENT,   zid_coeffs);
+    glMaterialfv(GL_FRONT, GL_DIFFUSE,   zid_coeffs);
+    glMaterialfv(GL_FRONT, GL_SPECULAR,  specular_coeffs);
+    glMaterialf(GL_FRONT,  GL_SHININESS, shininess);
 
 
-    glPushMatrix();
-
-    glColor3f(0.66, 0.66, 0.66);
-    glTranslatef(0+0.5, 1-0.5, 5); /*zid3*/
-    glScalef(1, 1, 10);
-    glutWireCube(1);
+    glScalef(15, 1, 1);
+    glutSolidCube(1);
     
     glPopMatrix();
 
 
     glPushMatrix();
 
-    glColor3f(0.66, 0.66, 0.66);
-    glTranslatef(9+0.5, 1-0.5, 5); /*zid4*/
-    glScalef(1, 1, 10);
-    glutWireCube(1);
+    glColor3f(0.36, 0.25, 0.26);
+    glTranslatef(0+0.5, 1-0.5, 6.5); /*zid3*/
+
+	glMaterialfv(GL_FRONT, GL_AMBIENT,   zid_coeffs);
+    glMaterialfv(GL_FRONT, GL_DIFFUSE,   zid_coeffs);
+    glMaterialfv(GL_FRONT, GL_SPECULAR,  specular_coeffs);
+    glMaterialf(GL_FRONT,  GL_SHININESS, shininess);
+
+
+
+    glScalef(1, 1, 13);
+    glutSolidCube(1);
     
     glPopMatrix();
 
+
+    glPushMatrix();
+
+    glColor3f(0.36, 0.25, 0.26);
+    glTranslatef(14+0.5, 1-0.5, 6.5); /*zid4*/
+
+	glMaterialfv(GL_FRONT, GL_AMBIENT,   zid_coeffs);
+    glMaterialfv(GL_FRONT, GL_DIFFUSE,   zid_coeffs);
+    glMaterialfv(GL_FRONT, GL_SPECULAR,  specular_coeffs);
+    glMaterialf(GL_FRONT,  GL_SHININESS, shininess);
+
+
+
+    glScalef(1, 1, 13);
+    glutSolidCube(1);
+    
+    glPopMatrix();
+
+
+/*STUBOVI*/
+int i,j;
+for(j=0;j<11;j=j+2)
+{
+	for(i=0;i<13; i=i+2)
+	{
+		glPushMatrix();
+
+		glColor3f(0.36, 0.25, 0.26);
+		glTranslatef(i+2+0.5, 1-0.5, j+2.5); 
+
+	glMaterialfv(GL_FRONT, GL_AMBIENT,   zid_coeffs);
+    glMaterialfv(GL_FRONT, GL_DIFFUSE,   zid_coeffs);
+    glMaterialfv(GL_FRONT, GL_SPECULAR,  specular_coeffs);
+    glMaterialf(GL_FRONT,  GL_SHININESS, shininess);
+
+		glScalef(1, 1, 1);
+		glutSolidCube(1);
+		
+		glPopMatrix();
+
+	}
+}
 
     glPushMatrix();
 
     glColor3f(0, 0, 0);
     glTranslatef(p2updown+7+0.5, 1-0.5, p2leftright+5); /*igrac1*/
+	glMaterialfv(GL_FRONT, GL_AMBIENT,   igrac_coeffs);
+    glMaterialfv(GL_FRONT, GL_DIFFUSE,   igrac_coeffs);
+    glMaterialfv(GL_FRONT, GL_SPECULAR,  specular_coeffs);
+    glMaterialf(GL_FRONT,  GL_SHININESS, shininess);
     glScalef(1, 1, 1);
-    glutWireSphere(0.4,10,10);
+    glutSolidSphere(0.4,10,10);
     
     glPopMatrix();	
 
@@ -251,8 +367,12 @@ static void on_display(void)
 
     glColor3f(0, 0, 0);
     glTranslatef(p1updown+2+0.5, 1-0.50, p1leftright+5);  /*igrac2*/
+	glMaterialfv(GL_FRONT, GL_AMBIENT,   igrac_coeffs);
+    glMaterialfv(GL_FRONT, GL_DIFFUSE,   igrac_coeffs);
+    glMaterialfv(GL_FRONT, GL_SPECULAR,  specular_coeffs);
+    glMaterialf(GL_FRONT,  GL_SHININESS, shininess);
     glScalef(1, 1, 1);
-    glutWireSphere(0.4,10,10);
+    glutSolidSphere(0.4,10,10);
     
     glPopMatrix();
 
